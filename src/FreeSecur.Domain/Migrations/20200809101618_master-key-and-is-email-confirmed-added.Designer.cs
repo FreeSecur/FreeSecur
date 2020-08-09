ca37c0980b991618070a8a1d031cb515e57b840a
Bram van Elderen <bramvanelderen1994@gmail.com>
2020-08-09 12:19:07 +0200

    added master key migration

--- src/FreeSecur.Domain/Migrations/20200809101618_master-key-and-is-email-confirmed-added.Designer.cs
+++ src/FreeSecur.Domain/Migrations/20200809101618_master-key-and-is-email-confirmed-added.Designer.cs
@@ -4,14 +4,16 @@ using FreeSecur.Domain;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FreeSecur.Domain.Migrations
 {
     [DbContext(typeof(FsDbContext))]
-    partial class FsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200809101618_master-key-and-is-email-confirmed-added")]
+    partial class masterkeyandisemailconfirmedadded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/FreeSecur.Domain/Migrations/20200809101618_master-key-and-is-email-confirmed-added.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FreeSecur.Domain.Migrations
+{
+    public partial class masterkeyandisemailconfirmedadded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MasterKey",
+                table: "Vaults",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsEmailConfirmed",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MasterKey",
+                table: "Vaults");
+
+            migrationBuilder.DropColumn(
+                name: "IsEmailConfirmed",
+                table: "Users");
+        }
+    }
+}
